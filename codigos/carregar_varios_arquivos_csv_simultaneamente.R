c5b8b30bb0a3568922427f296c5687919ad67e6d
John de Lara <johndelara1@gmail.com>
2019-07-26 16:08:38 -0300

    Criado Vizualização dos valores positivos e negativos

--- codigos/carregar_varios_arquivos_csv_simultaneamente.R
+++ codigos/carregar_varios_arquivos_csv_simultaneamente.R
@@ -5,3 +5,17 @@ class(lista_arquivos)
 lista_arquivos
 lista_arquivos2 <- lapply(lista_arquivos, read_csv)
 class(lista_arquivos2)
+i = 1
+while(i <= length(lista_arquivos2)){
+  if(i == 1){
+    contasJohn1 <- as.data.frame(lista_arquivos2[1])
+    contasJohn2 <- as.data.frame(lista_arquivos2[2])
+    contasJohn <- rbind(contasJohn1, contasJohn2)
+    i = 2
+  }else{
+    contasJohn3 <- as.data.frame(lista_arquivos2[i])
+    contasJohn <- rbind(contasJohn, contasJohn3)
+  }
+  i = i + 1;
+}
+
